db2e54bb7d465790b43b5e646540b70a5b252185
ZongHan <zonghan.ai12@nycu.edu.tw>
2024-06-04 19:59:07 +0800

    Add the function return the directory list
    Implement the simple directory listing feature.
    
    Server show the selcetd directory and get the diretory result when
    we use localhost:port in the blowser

--- http_server.c
+++ http_server.c
@@ -1,5 +1,6 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/fs.h>
 #include <linux/kthread.h>
 #include <linux/sched/signal.h>
 #include <linux/tcp.h>
@@ -7,30 +8,8 @@
 #include "http_parser.h"
 #include "http_server.h"
 
-#define CRLF "\r\n"
-
-#define HTTP_RESPONSE_200_DUMMY                               \
-    ""                                                        \
-    "HTTP/1.1 200 OK" CRLF "Server: " KBUILD_MODNAME CRLF     \
-    "Content-Type: text/plain" CRLF "Content-Length: 12" CRLF \
-    "Connection: Close" CRLF CRLF "Hello World!" CRLF
-#define HTTP_RESPONSE_200_KEEPALIVE_DUMMY                     \
-    ""                                                        \
-    "HTTP/1.1 200 OK" CRLF "Server: " KBUILD_MODNAME CRLF     \
-    "Content-Type: text/plain" CRLF "Content-Length: 12" CRLF \
-    "Connection: Keep-Alive" CRLF CRLF "Hello World!" CRLF
-#define HTTP_RESPONSE_501                                              \
-    ""                                                                 \
-    "HTTP/1.1 501 Not Implemented" CRLF "Server: " KBUILD_MODNAME CRLF \
-    "Content-Type: text/plain" CRLF "Content-Length: 21" CRLF          \
-    "Connection: Close" CRLF CRLF "501 Not Implemented" CRLF
-#define HTTP_RESPONSE_501_KEEPALIVE                                    \
-    ""                                                                 \
-    "HTTP/1.1 501 Not Implemented" CRLF "Server: " KBUILD_MODNAME CRLF \
-    "Content-Type: text/plain" CRLF "Content-Length: 21" CRLF          \
-    "Connection: KeepAlive" CRLF CRLF "501 Not Implemented" CRLF
-
 #define RECV_BUFFER_SIZE 4096
+#define SEND_BUFFER_SIZE 256
 
 #define MODULE_NAME "khttpd"
 
@@ -39,10 +18,13 @@ struct http_request {
     enum http_method method;
     char request_url[128];
     int complete;
+    struct dir_context dir_context;
 };
 
 struct khttpd_service daemon = {.is_stopped = false};
 struct workqueue_struct *khttpd_wq;  // define khttpd workqueue
+
+static bool handle_directory(struct http_request *request);
 
 static int http_server_recv(struct socket *sock, char *buf, size_t size)
 {
@@ -80,15 +62,12 @@ static int http_server_send(struct socket *sock, const char *buf, size_t size)
 
 static int http_server_response(struct http_request *request, int keep_alive)
 {
-    char *response;
-
-    pr_info("requested_url = %s\n", request->request_url);
-    if (request->method != HTTP_GET)
-        response = keep_alive ? HTTP_RESPONSE_501_KEEPALIVE : HTTP_RESPONSE_501;
-    else
-        response = keep_alive ? HTTP_RESPONSE_200_KEEPALIVE_DUMMY
-                              : HTTP_RESPONSE_200_DUMMY;
-    http_server_send(request->socket, response, strlen(response));
+    int ret = 0;
+    ret = handle_directory(request);
+    if (ret == 0) {
+        pr_err("handle_directory failed\n");
+        return -1;
+    }
     return 0;
 }
 
@@ -144,6 +123,66 @@ static int http_parser_callback_message_complete(http_parser *parser)
     http_server_response(request, http_should_keep_alive(parser));
     request->complete = 1;
     return 0;
+}
+// callback for 'iterate_dir', trace entry.
+static _Bool tracedir(struct dir_context *dir_context,
+                      const char *name,
+                      int namelen,
+                      loff_t offset,
+                      u64 ino,
+                      unsigned int d_type)
+{
+    if (strcmp(name, ".") && strcmp(name, "..")) {
+        struct http_request *request =
+            container_of(dir_context, struct http_request, dir_context);
+        char buf[SEND_BUFFER_SIZE] = {0};
+
+        snprintf(buf, SEND_BUFFER_SIZE,
+                 "<tr><td><a href=\"%s\">%s</a></td></tr>\r\n", name, name);
+        http_server_send(request->socket, buf, strlen(buf));
+    }
+    return true;
+}
+
+static bool handle_directory(struct http_request *request)
+{
+    struct file *fp;
+    char buf[SEND_BUFFER_SIZE] = {0};
+
+    request->dir_context.actor = tracedir;
+    if (request->method != HTTP_GET) {
+        snprintf(buf, SEND_BUFFER_SIZE,
+                 "HTTP/1.1 501 Not Implemented\r\n%s%s%s%s",
+                 "Content-Type: text/plain\r\n", "Content-Length: 19\r\n",
+                 "Connection: Close\r\n", "501 Not Implemented\r\n");
+        http_server_send(request->socket, buf, strlen(buf));
+        return false;
+    }
+
+    snprintf(buf, SEND_BUFFER_SIZE, "HTTP/1.1 200 OK\r\n%s%s%s",
+             "Connection: Keep-Alive\r\n", "Content-Type: text/html\r\n",
+             "Keep-Alive: timeout=5, max=1000\r\n\r\n");
+    http_server_send(request->socket, buf, strlen(buf));
+
+
+    snprintf(buf, SEND_BUFFER_SIZE, "%s%s%s%s", "<html><head><style>\r\n",
+             "body{font-family: monospace; font-size: 15px;}\r\n",
+             "td {padding: 1.5px 6px;}\r\n",
+             "</style></head><body><table>\r\n");
+    http_server_send(request->socket, buf, strlen(buf));
+
+    fp = filp_open("/home/deepcat/Documents/Course/linux2024/khttpd",
+                   O_RDONLY | O_DIRECTORY, 0);
+    if (IS_ERR(fp)) {
+        pr_info("Open file failed");
+        return false;
+    }
+
+    iterate_dir(fp, &request->dir_context);
+    snprintf(buf, SEND_BUFFER_SIZE, "</table></body></html>\r\n");
+    http_server_send(request->socket, buf, strlen(buf));
+    filp_close(fp, NULL);
+    return true;
 }
 
 // static int http_server_worker(void *arg)
